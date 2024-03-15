28557ed6b2c0c7d3baeab9da52d1ddf935326616
JL8L <159081567+YongxuSun1@users.noreply.github.com>
2024-03-15 09:08:24 +0000

    Update GRUmodel.py

--- GRUmodel.py
+++ GRUmodel.py
@@ -29,6 +29,7 @@ features = df.drop(['mean_temp'], axis=1)
 x_scaler = StandardScaler()
 features = x_scaler.fit_transform(features)
 
+
 def create_sequences(features, labels, time_steps):
     X, y = [], []
     for i in range(len(features) - time_steps):
@@ -36,31 +37,78 @@ def create_sequences(features, labels, time_steps):
         y.append(labels[i + time_steps])
     return np.array(X), np.array(y)
 
-time_steps = 10
+
+time_steps = 50
+batch_size = 32
+epochs = 50
+
 X, y = create_sequences(features, labels, time_steps)
+
+print(X.shape)
 
 y = y.reshape(-1,1)
 y_scaler = StandardScaler()
 y = y_scaler.fit_transform(y)
 
+
 # Split the dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-print(X_train.shape, y_train.shape)
+
+test_size = int(len(X) * 0.1)  # Split size is 0.1
+X_train = X[:-test_size]
+X_test = X[-test_size:]
+y_train = y[:-test_size]
+y_test = y[-test_size:]
+
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
 
 
 # Create the model
+
 model = Sequential([
-    GRU(64, input_shape=(time_steps, features.shape[1])),
-    Dense(1, activation='linear')
-    ])
+    GRU(64, activation='relu', input_shape=(time_steps, features.shape[1])),
+    Dense(1, activation='linear')])
 
 model.compile(optimizer=SGD(), loss='mse')
-batch_size = 32
-epochs = 100
 
-model . fit(X_train, y_train, batch_size=batch_size,
-            epochs=epochs, verbose=1, validation_split=0.1)
+checkpoint = ModelCheckpoint('GRU_best_model.keras',
+                             monitor='val_loss',
+                             verbose=1,
+                             save_best_only=True,
+                             mode='min')
+
+history = model . fit(X_train, y_train, batch_size=batch_size,
+            epochs=epochs, verbose=1, validation_split=0.1,callbacks=[checkpoint])
+
+# Plot the loss
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+
+plt.figure(figsize=(10, 6))
+plt.plot(epochs, loss, 'bo-', label='Training loss')
+plt.plot(epochs, val_loss, 'ro-', label='Validation loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
 
 mse = model . evaluate(X_test, y_test, verbose=0)
 
-print('MSE:', mse)
+predictions = model.predict(X_test)
+predictions = y_scaler.inverse_transform(predictions)
+y_test = y_scaler.inverse_transform(y_test)
+print('GRU MSE:', mse)
+
+# Plot the actual value and predicted value
+plt.figure(figsize=(10, 6))
+plt.scatter(range(len(y_test)), y_test, color='blue', label='Actual values')
+plt.scatter(range(len(predictions)), predictions, color='red', alpha=0.5, label='Predicted values')
+plt.title('Actual vs. Predicted values')
+plt.xlabel('Sample index')
+plt.ylabel('Value')
+plt.legend()
+plt.show()
